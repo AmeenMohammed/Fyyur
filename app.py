513cf594c7350ae38581dd1bcc27f5e95d1b1cac
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-09-20 22:34:51 +0200

    Add missing cols to venue model

--- app.py
+++ app.py
@@ -41,8 +41,10 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+    genres = db.Column(db.ARRAY(db.String))
+    seeking_talent = db.Column(db.Boolean(), default=False)
+    seeking_description = db.Column(db.String())
+    website_link = db.Column(db.String(500))
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
