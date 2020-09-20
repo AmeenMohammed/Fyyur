4e77336ba1b58dbfd97b4141d8a98a3057044e86
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-09-20 22:36:51 +0200

    Add missing cols to artist model

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String))
+    genres = db.Column(db.ARRAY(db.String()))
     seeking_talent = db.Column(db.Boolean(), default=False)
     seeking_description = db.Column(db.String())
     website_link = db.Column(db.String(500))
@@ -54,11 +54,13 @@ class Artist(db.Model):
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String()))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean(), default=False)
+    seeking_description = db.Column(db.String(300))
+    website_link = db.Column(db.String(500))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
