71cb61b13055172ac5dc3339328a87a0a7199dfb
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-09-21 15:16:37 +0200

    Add venues search functionality

--- app.py
+++ app.py
@@ -137,22 +137,32 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for Hop should return "The Musical Hop".
-  # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
-  response={
-    "count": 1,
-    "data": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_venues.html', results=response, search_term=request.form.get('search_term', ''))
+  search_term = request.form.get('search_term', '')
+  venues = Venue.query.filter(Venue.name.ilike(f"%{search_term}%")).all()
+  data = []
+
+  for venue in venues:
+    upcoming_shows = Venue.query \
+        .join(Shows) \
+        .filter(Shows.c.venue_id == venue.id) \
+        .filter(Shows.c.start_time > datetime.now()) \
+        .all()
+    data.append({
+      'id': venue.id,
+      'name': venue.name,
+      'num_upcoming_shows': len(upcoming_shows)
+    })
+    
+  response = {
+    'venues': data,
+    'count': len(venues)
+  }      
+
+  return render_template('pages/search_venues.html', results=response, search_term=search_term)
 
 @app.route('/venues/<int:venue_id>')
 def show_venue(venue_id):
-  
+
   venue = Venue.query.filter_by(id=venue_id).first()
   shows = db.session.query(Shows).filter(Shows.c.venue_id == venue.id).all()
   upcoming_shows = []
