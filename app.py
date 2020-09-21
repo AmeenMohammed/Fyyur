cd4522fe9250187c63b5cd34f62b2a58403daa8d
Ameen Mohammed <ameen.mohammed95by@gmail.com>
2020-09-21 05:13:27 +0200

    List added venues

--- app.py
+++ app.py
@@ -98,33 +98,42 @@ def index():
 
 #  Venues
 #  ----------------------------------------------------------------
-
 @app.route('/venues')
 def venues():
-  # TODO: replace with real venues data.
-  #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
-  return render_template('pages/venues.html', areas=data);
+  data = []
+
+  areas = Venue.query \
+      .with_entities(Venue.city, Venue.state) \
+      .group_by(Venue.city, Venue.state) \
+      .all()
+
+  for area in areas:
+    venues_data = []
+
+    venues = Venue.query \
+        .filter_by(state=area.state) \
+        .filter_by(city=area.city) \
+        .all()
+
+    for venue in venues:
+      upcoming_shows = Venue.query.join(Shows)\
+                    .filter(Shows.c.start_time > datetime.now())\
+                    .filter(Shows.c.venue_id == venue.id)\
+                    .all()
+
+      venues_data.append({
+          'id': venue.id,
+          'name': venue.name,
+          'num_upcoming_shows': len(upcoming_shows)
+      })
+
+    data.append({
+        'city': area.city,
+        'state': area.state,
+        'venues': venues_data
+    })
+
+  return render_template('pages/venues.html', areas=data)
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
